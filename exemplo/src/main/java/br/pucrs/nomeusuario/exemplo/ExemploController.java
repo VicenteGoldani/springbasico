b7e9aca502291db1b263b0bc3bc2e0474d4d7a31
Luisa <lulummmacedo@gmail.com>
2026-09-15 01:00:05 +0000

    Corrige controller e Livro com os endpoints de parametros

--- exemplo/src/main/java/br/pucrs/nomeusuario/exemplo/ExemploController.java
+++ exemplo/src/main/java/br/pucrs/nomeusuario/exemplo/ExemploController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/biblioteca")
@@ -47,6 +53,43 @@ public class ExemploController {
                 .map(l -> l.getAutor())
                 .distinct()
                 .toList();
+    }
+
+    // Query string: /biblioteca/livrosautor?autor=Maria da Silva
+    @GetMapping("/livrosautor")
+    public List<Livro> getLivrosDoAutor(@RequestParam(value = "autor") String autor) {
+        return livros.stream()
+                .filter(livro -> livro.getAutor().equals(autor))
+                .toList();
+    }
+
+    // Path parameters: /biblioteca/livrosautorano/{autor}/ano/{ano}
+    @GetMapping("/livrosautorano/{autor}/ano/{ano}")
+    public List<Livro> getLivrosDoAutorEAno(@PathVariable(value = "autor") String autor,
+            @PathVariable(value = "ano") int ano) {
+        return livros.stream()
+                .filter(livro -> livro.getAutor().equals(autor))
+                .filter(livro -> livro.getAno() == ano)
+                .toList();
+    }
+
+    // Body data: POST /biblioteca/novolivro
+    @PostMapping("/novolivro")
+    public boolean cadastraLivroNovo(@RequestBody final Livro livro) {
+        livros.add(livro);
+        return true;
+    }
+
+    // Response Entity: /biblioteca/livrotitulo/{titulo}
+    @GetMapping("/livrotitulo/{titulo}")
+    public ResponseEntity<Livro> getLivroTitulo(@PathVariable("titulo") String titulo) {
+        Livro resp = livros.stream()
+                .filter(livro -> livro.getTitulo().equals(titulo))
+                .findFirst()
+                .orElse(null);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(resp);
     }
 
 }
